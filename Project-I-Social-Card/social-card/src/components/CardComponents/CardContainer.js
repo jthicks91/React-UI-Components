83e6724d0e3b1af26839d316dba9e0627b81d49d
jthicks91 <jordanthicks91@gmail.com>
2019-02-27 14:09:15 -0800

    Finished adding content for Card Container via JSX and React. Will style the card now to the design file via CSS.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+
+import "./Card.css";
+
+import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
+
+const CardContainer = () => {
+  return (
+    <body>
+      <CardBanner />
+      <CardContent />
+    </body>
+  );
+};
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,18 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = () => {
+  return (
+    <section className="CardContent">
+      <h2>Get started with React</h2>
+      <p>
+        React makes it painless to create interactive UIs. Design simpkle views
+        for <br />
+        each state in your application.{" "}
+      </p>
+      <a href="reactjs.org">React JS</a>
+    </section>
+  );
+};
+
+export default CardContent;
