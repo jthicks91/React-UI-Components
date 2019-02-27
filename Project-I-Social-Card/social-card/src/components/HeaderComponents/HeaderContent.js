ca328334d5368cb6a88513d6f26faf1dfa22c1c1
jthicks91 <jordanthicks91@gmail.com>
2019-02-27 13:50:40 -0800

    Set up React enviornment for project. Finished creating HeaderContainer via React and JSX. Card Container via these same tools are next.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,26 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+
+import "./Header.css";
+
+import HeaderTitle from "./HeaderTitle";
+
+const HeaderContent = () => {
+  return (
+    <section className="headerContent">
+      <div>
+        <h1>Lambda School</h1>
+        <HeaderTitle />
+      </div>
+
+      <p>
+        Let's learn React by building simple interfaces with components. Dont
+        try to
+        <br /> overthink it, just keep it simple and have fun. Once you feel
+        comfortable using
+        <br /> components you are well on your way to mastering React!
+      </p>
+    </section>
+  );
+};
+
+export default HeaderContent;
